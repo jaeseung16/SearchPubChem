dec65a340526713f7cc1ef18e2936c09be01be86
Jae-Seung Lee <jaeseung@gmail.com>
2018-01-17 18:50:01 -0500

    Removed downloadSample(compound:) Implemented searchByName(_:)

--- SearchPubChem/ChemicalTableViewController.swift
+++ SearchPubChem/ChemicalTableViewController.swift
@@ -21,7 +21,6 @@ class ChemicalTableViewController: UITableViewController {
         compounds = appDelegate.compounds
         self.tableView.reloadData()
         
-        downloadSample(compound: "chondroitin sulfate")
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -107,96 +106,5 @@ class ChemicalTableViewController: UITableViewController {
 }
 
 extension ChemicalTableViewController {
-    func downloadSample(compound name: String) {
-        let properties = ["MolecularFormula", "MolecularWeight", "IUPACName"]
-        var pathForProperties = "/property/"
-        
-        for property in properties {
-            pathForProperties += property + ","
-        }
-        
-        pathForProperties.remove(at: pathForProperties.index(before: pathForProperties.endIndex))
-        pathForProperties += "/json"
-        
-        var component = URLComponents()
-        component.scheme = "https"
-        component.host = "pubchem.ncbi.nlm.nih.gov"
-        component.path = "/rest/pug/compound/name/" + name + pathForProperties
-        
-        print("\(String(describing: component.url))")
-        let request = URLRequest(url: component.url!)
-        
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            func sendError(_ error: String) {
-                let userInfo = [NSLocalizedDescriptionKey: error]
-                print("Error: \(String(describing: userInfo[NSLocalizedDescriptionKey]))")
-            }
-            
-            guard (error == nil) else {
-                sendError("There was an error with your request: \(String(describing: error))!")
-                return
-            }
-            
-            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
-                let statusCode = (response as? HTTPURLResponse)!.statusCode
-                var errorString: String
-                
-                switch(statusCode) {
-                case 400:
-                    errorString = "Request is improperly formed!"
-                case 404:
-                    errorString = "The input record was not found!"
-                case 405:
-                    errorString = "Request not allowed!"
-                case 503:
-                    errorString = "Too many requests or server is busy!"
-                case 504:
-                    errorString = "The request timed out!"
-                default:
-                    errorString = "Your request returned a stauts code other than 2xx!"
-                }
-                
-                sendError(errorString)
-                return
-            }
-            
-            guard let data = data else {
-                sendError("No data was returned by the request!")
-                return
-            }
-            
-            let parsedResult: [String: AnyObject]!
-            
-            do {
-                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String: AnyObject]
-            } catch {
-                print("Cannot parse JSON!")
-                return
-            }
-            
-            print("\(parsedResult)")
-            
-            guard let propertyTable = parsedResult["PropertyTable"] as? [String: AnyObject] else {
-                sendError("There is no PropertyTable in: \(parsedResult)")
-                return
-            }
-            
-            guard let properties = propertyTable["Properties"] as? [[String: AnyObject]] else {
-                sendError("There is no properties in: \(propertyTable)")
-                return
-            }
-            
-            let CID = properties[0]["CID"] as! Int
-            let molecularFormula = properties[0]["MolecularFormula"] as! String
-            let molecularWeight = properties[0]["MolecularWeight"] as! Double
-            let nameIUPAC = properties[0]["IUPACName"] as! String
-            
-            print("CID: \(CID)\n")
-            print("Molecular Formula: \(molecularFormula)")
-            print("Molecular Weight: \(molecularWeight)")
-            print("IUPAC Name: \(nameIUPAC)")
-        }
-        
-        task.resume()
-    }
+
 }
--- SearchPubChem/SearchByNameViewController.swift
+++ SearchPubChem/SearchByNameViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SearchByNameViewController: UIViewController {
 
+    @IBOutlet weak var nameToSearch: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +23,105 @@ class SearchByNameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func searchByName(_ sender: UIButton) {
+        let name = nameToSearch.text!
+        
+        let properties = ["MolecularFormula", "MolecularWeight", "IUPACName"]
+        var pathForProperties = "/property/"
+        
+        for property in properties {
+            pathForProperties += property + ","
+        }
+        
+        pathForProperties.remove(at: pathForProperties.index(before: pathForProperties.endIndex))
+        pathForProperties += "/json"
+        
+        var component = URLComponents()
+        component.scheme = "https"
+        component.host = "pubchem.ncbi.nlm.nih.gov"
+        component.path = "/rest/pug/compound/name/" + name + pathForProperties
+        
+        print("\(String(describing: component.url))")
+        let request = URLRequest(url: component.url!)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            func sendError(_ error: String) {
+                let userInfo = [NSLocalizedDescriptionKey: error]
+                print("Error: \(String(describing: userInfo[NSLocalizedDescriptionKey]))")
+            }
+            
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(String(describing: error))!")
+                return
+            }
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                let statusCode = (response as? HTTPURLResponse)!.statusCode
+                var errorString: String
+                
+                switch(statusCode) {
+                case 400:
+                    errorString = "Request is improperly formed!"
+                case 404:
+                    errorString = "The input record was not found!"
+                case 405:
+                    errorString = "Request not allowed!"
+                case 503:
+                    errorString = "Too many requests or server is busy!"
+                case 504:
+                    errorString = "The request timed out!"
+                default:
+                    errorString = "Your request returned a stauts code other than 2xx!"
+                }
+                
+                sendError(errorString)
+                return
+            }
+            
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            let parsedResult: [String: AnyObject]!
+            
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String: AnyObject]
+            } catch {
+                print("Cannot parse JSON!")
+                return
+            }
+            
+            print("\(parsedResult)")
+            
+            guard let propertyTable = parsedResult["PropertyTable"] as? [String: AnyObject] else {
+                sendError("There is no PropertyTable in: \(parsedResult)")
+                return
+            }
+            
+            guard let properties = propertyTable["Properties"] as? [[String: AnyObject]] else {
+                sendError("There is no properties in: \(propertyTable)")
+                return
+            }
+            
+            let CID = properties[0]["CID"] as! Int
+            let molecularFormula = properties[0]["MolecularFormula"] as! String
+            let molecularWeight = properties[0]["MolecularWeight"] as! Double
+            let nameIUPAC = properties[0]["IUPACName"] as! String
+            
+            print("CID: \(CID)")
+            print("Molecular Formula: \(molecularFormula)")
+            print("Molecular Weight: \(molecularWeight)")
+            print("IUPAC Name: \(nameIUPAC)")
+        }
+        
+        task.resume()
+    }
+    
+    @IBAction func dismiss(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
