43abe84876f4fa0f16dd63ec00994d862dfe9565
Jae-Seung Lee <jaeseung@gmail.com>
2019-07-21 15:18:40 -0400

    feat: Create Element.swift

---
+++ SearchPubChem/Element.swift
@@ -0,0 +1,24 @@
+//
+//  Element.swift
+//  SearchPubChem
+//
+//  Created by Jae Seung Lee on 7/21/19.
+//  Copyright © 2019 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Element {
+    var atomicNumber: Int
+    var name: String
+    var radius: Int // covalent radius from https://en.wikipedia.org/wiki/Covalent_radius
+    // check https://en.wikipedia.org/wiki/Atomic_radius
+    // check https://en.wikipedia.org/wiki/Atomic_radii_of_the_elements_(data_page)
+    
+    init() {
+        atomicNumber = 1
+        name = "hydrogen"
+        radius = 31
+    }
+}
