e5c7c705fad46c112230381b6fb993f91f701673
Jae-Seung Lee <jaeseung@gmail.com>
2019-07-21 15:10:10 -0400

    feat: Create Conformer.swift

---
+++ SearchPubChem/Conformer.swift
@@ -0,0 +1,13 @@
+//
+//  Conformer.swift
+//  SearchPubChem
+//
+//  Created by Jae Seung Lee on 7/21/19.
+//  Copyright © 2019 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+
+class Conformer {
+    
+}
