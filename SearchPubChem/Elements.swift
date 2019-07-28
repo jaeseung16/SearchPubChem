cfeaa7406d02b8a0ed9b60bd85253a8212d3e931
Jae-Seung Lee <jaeseung@gmail.com>
2019-07-28 16:24:15 -0400

    feat: Create Elements.swift

---
+++ SearchPubChem/Elements.swift
@@ -0,0 +1,43 @@
+//
+//  Elements.swift
+//  SearchPubChem
+//
+//  Created by Jae Seung Lee on 7/28/19.
+//  Copyright © 2019 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+
+enum Elements: Int {
+    case hydrogen = 1
+    case helium
+    case lithium
+    case berylium
+    case boron
+    case carbon
+    case nitrogen
+    case oxygen
+    case fluorine
+    case neon
+    
+    public func getElement() -> Element {
+        var elementToReturn = Element()
+        elementToReturn.atomicNumber = self.rawValue
+        switch(self) {
+        case .hydrogen:
+            elementToReturn.name = "hydrogen"
+            elementToReturn.radius = 120
+        case .carbon:
+            elementToReturn.name = "carbon"
+            elementToReturn.radius = 170
+        case .oxygen:
+            elementToReturn.name = "oxygen"
+            elementToReturn.radius = 152
+        default:
+            elementToReturn.name = ""
+            elementToReturn.radius = 0
+        }
+        
+        return elementToReturn;
+    }
+}
