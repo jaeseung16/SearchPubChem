d4dc6b7472b7b3a4d75c1649efa1138099d7e413
Jae-Seung Lee <jaeseung@gmail.com>
2020-07-04 20:47:28 -0400

    Create CompoundDTO

---
+++ SearchPubChem/Compounds/CompoundDTO.swift
@@ -0,0 +1,32 @@
+//
+//  CompoundDTO.swift
+//  SearchPubChem
+//
+//  Created by Jae Seung Lee on 7/4/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+
+struct CompoundDTO: Codable {
+    enum CodingKeys: String, CodingKey {
+        case propertyTable = "PropertyTable"
+    }
+    
+    var propertyTable: PropertyTable
+}
+
+struct PropertyTable: Codable {
+    enum CodingKeys: String, CodingKey {
+        case properties = "Properties"
+    }
+    
+    var properties: [Properties]
+}
+
+struct Properties: Codable {
+    var CID: Int
+    var IUPACName: String
+    var MolecularFormula: String
+    var MolecularWeight: Double
+}
