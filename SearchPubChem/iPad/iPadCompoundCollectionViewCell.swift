edf182c1ace03ada6d677fbfb57b8511ff176318
Jae-Seung Lee <jaeseung@gmail.com>
2020-07-19 18:20:46 -0400

    Add iPadCompoundCollectionViewCell.swift

---
+++ SearchPubChem/iPad/iPadCompoundCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  iPadCompoundCollectionViewCell.swift
+//  SearchPubChem
+//
+//  Created by Jae Seung Lee on 7/19/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import UIKit
+
+class iPadCompoundCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var compoundImageView: UIImageView!
+    @IBOutlet weak var compoundNameLabel: UILabel!
+}
