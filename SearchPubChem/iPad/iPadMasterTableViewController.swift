5621df2dce5468452e666f3bc608725c058dbd38
Jae-Seung Lee <jaeseung@gmail.com>
2020-08-01 14:16:38 -0400

    Update iPadMasterTableViewController.swift
    Clean up

--- SearchPubChem/iPad/iPadMasterTableViewController.swift
+++ SearchPubChem/iPad/iPadMasterTableViewController.swift
@@ -10,37 +10,29 @@ import UIKit
 import CoreData
 
 class iPadMasterTableViewController: UITableViewController {
-    
+    // MARK: - Properties
+    // Constants
     let menuItems = ["Compounds", "Solutions"]
+    let collectionViewControllerIdentifier = "iPadCompoundCollectionViewController"
+    let solutionViewControllerIdentifier = "SolutionTableViewController"
     
+    // Variables
     var compoundCollectionViewController: iPadCompoundCollectionViewController?
     var solutionTableViewController: SolutionTableViewController?
     var dataController: DataController!
     var compounds = [Compound]()
-    let collectionViewControllerIdentifier = "iPadCompoundCollectionViewController"
-    let solutionViewControllerIdentifier = "SolutionTableViewController"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         setupCompoundCollectionViewController()
         setupSolutionCollectionViewController()
     }
     
     func setupCompoundCollectionViewController() -> Void {
-        print("setupCompoundCollectionViewController")
         compoundCollectionViewController = UIStoryboard(name: "iPad", bundle: nil).instantiateViewController(withIdentifier: collectionViewControllerIdentifier) as? iPadCompoundCollectionViewController
                
         compoundCollectionViewController?.dataController = dataController
-        //compoundCollectionViewController?.fetchedResultsController = setupFetchedResultsControllerForCompound()
-        //compoundCollectionViewController?.delegate = self
-        //compoundCollectionViewController?.compounds = compounds
     }
     
     func setupSolutionCollectionViewController() -> Void {
@@ -48,25 +40,13 @@ class iPadMasterTableViewController: UITableViewController {
                
         solutionTableViewController?.dataController = dataController
     }
-    
-    func setupFetchedResultsControllerForCompound() -> NSFetchedResultsController<Compound> {
-        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
-        
-        let fetchRequest: NSFetchRequest<Compound> = Compound.fetchRequest()
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        
-        return NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: "firstCharacterInName", cacheName: "compounds")
-    }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return menuItems.count
     }
 
@@ -98,51 +78,6 @@ class iPadMasterTableViewController: UITableViewController {
             }
         }
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
