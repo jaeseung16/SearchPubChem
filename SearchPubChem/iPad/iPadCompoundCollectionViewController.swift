2950e49dee03828e9a0ce4b7660c86fcd177ba01
Jae-Seung Lee <jaeseung@gmail.com>
2021-05-21 17:51:41 -0400

    Feature: Show tags with compounds

--- SearchPubChem/iPad/iPadCompoundCollectionViewController.swift
+++ SearchPubChem/iPad/iPadCompoundCollectionViewController.swift
@@ -15,17 +15,27 @@ class iPadCompoundCollectionViewController: UIViewController {
     @IBOutlet weak var compoundCollectionView: UICollectionView!
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
+    @IBOutlet weak var tagCollectionView: UICollectionView!
+    @IBOutlet weak var tagCollectionViewFlowLayout: UICollectionViewFlowLayout!
+    
     let collectionViewCellIdentifier = "iPadCompoundCollectionViewCell"
+    let tagCollectionViewCellIdentifier = "tagCollectionViewCell"
     let detailViewControllerIdentifier = "iPadCompoundDetailViewController"
 
     var dataController: DataController!
     var fetchedResultsController: NSFetchedResultsController<Compound>!
+    var tagFetchedResultsController: NSFetchedResultsController<CompoundTag>!
+    
+    var selectedTag: CompoundTag?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUpFetchedResultsController()
         adjustFlowLayoutSize(size: view.frame.size)
+        
+        setUpTagFetchedResultsController()
+        print("tagCollectionViewFlowLayout.itemSize = \(tagCollectionViewFlowLayout.itemSize)")
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -53,6 +63,28 @@ class iPadCompoundCollectionViewController: UIViewController {
         return fetchRequest
     }
     
+    func setUpTagFetchedResultsController() {
+        let fetchRequest: NSFetchRequest<CompoundTag> = setupTagFetchRequest()
+        
+        tagFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: "compoundTags")
+        tagFetchedResultsController.delegate = self
+        
+        do {
+            try tagFetchedResultsController.performFetch()
+        } catch {
+            fatalError("Compounds cannot be fetched: \(error.localizedDescription)")
+        }
+    }
+    
+    func setupTagFetchRequest() -> NSFetchRequest<CompoundTag> {
+        let fetchRequest: NSFetchRequest<CompoundTag> = CompoundTag.fetchRequest()
+        let countSortDescriptor = NSSortDescriptor(key: "compoundCount", ascending: false)
+        let nameSortDescriptor = NSSortDescriptor(key: "name", ascending: true, selector: #selector(NSString.caseInsensitiveCompare))
+        
+        fetchRequest.sortDescriptors = [countSortDescriptor, nameSortDescriptor]
+        return fetchRequest
+    }
+    
     // MARK: - Navigation
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -66,37 +98,154 @@ class iPadCompoundCollectionViewController: UIViewController {
 // MARK: - UICollectionViewDelegate, UICollectionViewDataSource
 extension iPadCompoundCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return fetchedResultsController.sections?.count ?? 0
+        if collectionView == compoundCollectionView {
+            return fetchedResultsController.sections?.count ?? 0
+        } else {
+            return tagFetchedResultsController.sections?.count ?? 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+        if collectionView == compoundCollectionView {
+            return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+        } else {
+            return tagFetchedResultsController.sections?[section].numberOfObjects ?? 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let compound = fetchedResultsController.object(at: indexPath)
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewCellIdentifier, for: indexPath) as! iPadCompoundCollectionViewCell
-        
-        cell.compoundNameLabel.text = compound.name
-        
-        if let imageData = compound.image {
-            cell.compoundImageView.image = UIImage(data: imageData as Data)
+        if collectionView == compoundCollectionView {
+            let compound = fetchedResultsController.object(at: indexPath)
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewCellIdentifier, for: indexPath) as! iPadCompoundCollectionViewCell
+            
+            cell.compoundNameLabel.text = compound.name
+            
+            if let imageData = compound.image {
+                cell.compoundImageView.image = UIImage(data: imageData as Data)
+            }
+            
+            return cell
+        } else {
+            let tag = tagFetchedResultsController.object(at: indexPath)
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: tagCollectionViewCellIdentifier, for: indexPath) as! TagCollectionViewCell
+            
+            cell.nameLabel.text = tag.name
+            cell.countLabel.text = "\(tag.compoundCount)"
+            
+            return cell
         }
         
-        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
+        var shouldSelect = true
+        
+        if collectionView == tagCollectionView {
+            let tag = tagFetchedResultsController.object(at: indexPath)
+            
+            if (selectedTag == tag) {
+                
+                selectedTag = nil
+               
+                if collectionView == tagCollectionView {
+                    if let cell = collectionView.cellForItem(at: indexPath) as? TagCollectionViewCell {
+                        cell.contentView.backgroundColor = .white
+                    }
+                }
+                
+                fetchedResultsController.delegate = nil
+                
+                setUpFetchedResultsController()
+                
+                compoundCollectionView.reloadData()
+                
+                shouldSelect = false
+            } else {
+                let tag = tagFetchedResultsController.object(at: indexPath)
+                selectedTag = tag
+                
+                let sortDescriptor = NSSortDescriptor(key: "name", ascending: true, selector: #selector(NSString.caseInsensitiveCompare))
+                let predicate = NSPredicate(format: "tags CONTAINS %@", argumentArray: [tag])
+                
+                let fetchRequest: NSFetchRequest<Compound> = Compound.fetchRequest()
+                fetchRequest.sortDescriptors = [sortDescriptor]
+                fetchRequest.predicate = predicate
+                
+                fetchedResultsController.delegate = nil
+                
+                fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: "firstCharacterInName", cacheName: nil)
+                fetchedResultsController.delegate = self
+                
+                do {
+                    try fetchedResultsController.performFetch()
+                } catch {
+                    fatalError("Compounds cannot be fetched: \(error.localizedDescription)")
+                }
+                
+                compoundCollectionView.reloadData()
+            }
+        }
+        return shouldSelect
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let compound = fetchedResultsController.object(at: indexPath)
-        let detailViewController = setupDetailViewController(for: compound)
-        detailViewController.delegate = self
-        navigationController?.pushViewController(detailViewController, animated: true)
-        collectionView.deselectItem(at: indexPath, animated: false)
+        if collectionView == compoundCollectionView {
+            let compound = fetchedResultsController.object(at: indexPath)
+            let detailViewController = setupDetailViewController(for: compound)
+            detailViewController.delegate = self
+            navigationController?.pushViewController(detailViewController, animated: true)
+            collectionView.deselectItem(at: indexPath, animated: false)
+        } else {
+            if let cell = collectionView.cellForItem(at: indexPath) as? TagCollectionViewCell {
+                cell.contentView.backgroundColor = .cyan
+            }
+        }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        if collectionView == tagCollectionView {
+            if let cell = collectionView.cellForItem(at: indexPath) as? TagCollectionViewCell {
+                cell.contentView.backgroundColor = .white
+            }
+        }
+    }
+    
+    /*
+    func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        if collectionView == compoundCollectionView {
+            let compound = fetchedResultsController.object(at: indexPath)
+            
+            guard let selected = selectedTag, let tags = compound.tags else {
+                return false
+            }
+            
+            if tags.contains(selected) {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewCellIdentifier, for: indexPath) as! iPadCompoundCollectionViewCell
+                cell.compoundNameLabel.backgroundColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
+                
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return true
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        if collectionView == compoundCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewCellIdentifier, for: indexPath) as! iPadCompoundCollectionViewCell
+            cell.contentView.backgroundColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        if collectionView == compoundCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewCellIdentifier, for: indexPath) as! iPadCompoundCollectionViewCell
+            cell.contentView.backgroundColor = nil
+        }
+    }
+    */
     
     func setupDetailViewController(for compound: Compound) -> iPadCompoundDetailViewController {
         let fetchRequest = buildSolutionFetchRequest(for: compound)
@@ -162,9 +311,17 @@ extension iPadCompoundCollectionViewController: NSFetchedResultsControllerDelega
         
         switch type {
         case .insert:
-            compoundCollectionView.insertSections(set)
+            if controller == fetchedResultsController {
+                compoundCollectionView.insertSections(set)
+            } else {
+                tagCollectionView.insertSections(set)
+            }
         case .delete:
-            compoundCollectionView.deleteSections(set)
+            if controller == fetchedResultsController {
+                compoundCollectionView.deleteSections(set)
+            } else {
+                tagCollectionView.deleteSections(set)
+            }
         default:
             break
         }
@@ -173,14 +330,31 @@ extension iPadCompoundCollectionViewController: NSFetchedResultsControllerDelega
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
         switch type {
         case .insert:
-            compoundCollectionView.insertItems(at: [newIndexPath!])
+            if controller == fetchedResultsController {
+                compoundCollectionView.insertItems(at: [newIndexPath!])
+            } else {
+                tagCollectionView.insertItems(at: [newIndexPath!])
+            }
         case .delete:
-            compoundCollectionView.deleteItems(at: [indexPath!])
+            if controller == fetchedResultsController {
+                compoundCollectionView.deleteItems(at: [indexPath!])
+            } else {
+                tagCollectionView.deleteItems(at: [indexPath!])
+            }
         case .update:
-            compoundCollectionView.reloadItems(at: [indexPath!])
+            if controller == fetchedResultsController {
+                compoundCollectionView.reloadItems(at: [indexPath!])
+            } else {
+                tagCollectionView.reloadItems(at: [indexPath!])
+            }
         case .move:
-            compoundCollectionView.deleteItems(at: [indexPath!])
-            compoundCollectionView.insertItems(at: [newIndexPath!])
+            if controller == fetchedResultsController {
+                compoundCollectionView.deleteItems(at: [indexPath!])
+                compoundCollectionView.insertItems(at: [newIndexPath!])
+            } else {
+                tagCollectionView.deleteItems(at: [indexPath!])
+                tagCollectionView.insertItems(at: [newIndexPath!])
+            }
         @unknown default:
             fatalError()
         }
