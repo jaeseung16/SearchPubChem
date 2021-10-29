d12fd26b2d6126074d815c0f7e6bfe6aac55667a
Jae-Seung Lee <jaeseung@gmail.com>
2021-10-29 18:01:00 -0400

    Fix: Context in environment is not connected to a persistent Stor coordinator

--- SearchPubChem/Views/MakeSolutionView.swift
+++ SearchPubChem/Views/MakeSolutionView.swift
@@ -74,6 +74,7 @@ struct MakeSolutionView: View {
         }
         .sheet(isPresented: $presentSelectCompoundsView) {
             SelectCompoundsView(selectedCompounds: selectedCompounds)
+                .environment(\.managedObjectContext, viewContext)
         }
     }
     
