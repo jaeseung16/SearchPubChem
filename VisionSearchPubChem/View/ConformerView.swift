2dd2f6b7e198a18e9753d22519dd12e15fcfc1bf
Jae-Seung Lee <jaeseung@gmail.com>
2024-03-02 18:57:36 -0500

    Feature: Rotate conformer by dragging

--- VisionSearchPubChem/View/ConformerView.swift
+++ VisionSearchPubChem/View/ConformerView.swift
@@ -16,60 +16,78 @@ struct ConformerView: View {
     
     @State private var entity: Entity?
     
+    @State private var totalRotation3D = Rotation3D.identity
+    @State private var rotationAxis = RotationAxis3D(x: 0, y: 0, z: 0)
+    @State private var rotationAngle = Angle.zero
+    
     var body: some View {
         GeometryReader3D { geometry in
-            RealityView { content in
-                guard let conformer else {
-                    return
-                }
-                
-                let maxLocation = conformer.atoms.compactMap {
-                    $0.location.map { abs($0) }
-                        .max()
-                }.max()
-                
-                guard let maxLocation else {
-                    return
-                }
-                
-                let bounds = content.convert(geometry.frame(in: .local), from: .local, to: content)
-                
-                let scale = bounds.extents.min() / Float(3 * maxLocation)
-                
-                let boxMeshResource = MeshResource.generateBox(size: bounds.extents.min())
-                let boxShapeResource = ShapeResource.generateBox(size: bounds.extents)
-                let boxEntity = ModelEntity(mesh: boxMeshResource, materials: [UnlitMaterial(color: .clear)], collisionShape: boxShapeResource, mass: 0.0)
-                
-                for atom in conformer.atoms {
-                    guard let element = Elements(rawValue: atom.number) else {
-                        print("No such element: atomic number = \(atom.number)")
+            ZStack(alignment: .bottom) {
+                RealityView { content in
+                    guard let conformer else {
                         return
                     }
-
-                    let radius = element.getVanDerWaalsRadius() > 0 ? element.getVanDerWaalsRadius() : element.getCovalentRadius()
                     
-                    let sphereMeshResource = MeshResource.generateSphere(radius: Float(radius) * scale / 200.0)
-                    let sphereEntity = ModelEntity(mesh: sphereMeshResource, materials: [SimpleMaterial(color: element.getColor(), roughness: 0, isMetallic: false)])
-                    let location = atom.location.map { Float($0) * scale }
-                    sphereEntity.position = SIMD3<Float>(x: location[0], y: location[1], z: location[2])
-                    //sphereEntity.components[OpacityComponent.self] = .init(opacity: 1.0)
-                    sphereEntity.setParent(boxEntity)
+                    let maxLocation = conformer.atoms.compactMap {
+                        $0.location.map { abs($0) }
+                            .max()
+                    }.max()
+                    
+                    guard let maxLocation else {
+                        return
+                    }
+                    
+                    let bounds = content.convert(geometry.frame(in: .local), from: .local, to: content)
+                    
+                    let scale = bounds.extents.min() / Float(3 * maxLocation)
+                    
+                    let boxMeshResource = MeshResource.generateBox(size: bounds.extents.min())
+                    let boxShapeResource = ShapeResource.generateBox(size: bounds.extents)
+                    let boxEntity = ModelEntity(mesh: boxMeshResource, materials: [UnlitMaterial(color: .clear)], collisionShape: boxShapeResource, mass: 0.0)
+                    
+                    for atom in conformer.atoms {
+                        guard let element = Elements(rawValue: atom.number) else {
+                            print("No such element: atomic number = \(atom.number)")
+                            return
+                        }
+
+                        let radius = element.getVanDerWaalsRadius() > 0 ? element.getVanDerWaalsRadius() : element.getCovalentRadius()
+                        let location = atom.location.map { Float($0) * scale }
+                        
+                        let sphereMeshResource = MeshResource.generateSphere(radius: Float(radius) * scale / 200.0)
+                        let sphereEntity = ModelEntity(mesh: sphereMeshResource, materials: [SimpleMaterial(color: element.getColor(), roughness: 0, isMetallic: false)])
+                        sphereEntity.position = SIMD3<Float>(x: location[0], y: location[1], z: location[2])
+                        sphereEntity.generateCollisionShapes(recursive: false)
+                        sphereEntity.components[InputTargetComponent.self] = .init()
+                        sphereEntity.setParent(boxEntity)
+                    }
+                    
+                    content.add(boxEntity)
+                    
+                    entity = boxEntity
+                } update: { content in
+                    
+                } placeholder: {
+                    ProgressView()
                 }
+                .rotation3DEffect(rotationAngle, axis: rotationAxis)
                 
-                content.add(boxEntity)
-                
-                entity = boxEntity
-                
-            } update: { content in
-                
-            } placeholder: {
-                ProgressView()
+                Text("\(compound?.name ?? "")")
+                    .font(.extraLargeTitle)
+                    .offset(y: -20)
             }
+            .gesture(
+                DragGesture()
+                    .targetedToAnyEntity()
+                    .onChanged{ value in
+                        rotate(by: value.convert(value.translation3D, from: .local, to: .scene), ended: false)
+                    }
+                    .onEnded { value in
+                        rotate(by: value.convert(value.translation3D, from: .local, to: .scene),  ended: true)
+                    }
+            )
             .hoverEffect()
-            .dragRotation(yawLimit: .degrees(20), pitchLimit: .degrees(20))
         }
-        
-        
     }
     
     private var conformer: Conformer? {
@@ -93,4 +111,20 @@ struct ConformerView: View {
         }
         return nil
     }
+    
+    private func rotate(by translation3D: SIMD3<Float>, ended: Bool) {
+        let translation2D = Vector3D(x: translation3D.y, y: translation3D.x, z: 0.0)
+        let newRotation3D = Rotation3D(angle: Angle2D(radians: translation2D.length * .pi),
+                                       axis: RotationAxis3D(x: translation2D.x, y: translation2D.y, z: 0))
+        
+        let newTotalRotation3D = newRotation3D * totalRotation3D
+        
+        rotationAngle = Angle(radians: newTotalRotation3D.angle.radians)
+        rotationAxis = newTotalRotation3D.axis
+        
+        if ended {
+            totalRotation3D = newTotalRotation3D
+        }
+    }
+    
 }
